8c440262728db6b149827d4a75d89f95fa741855
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-15 21:22:18 +0530

    Writing Expectations

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -2,3 +2,13 @@ import 'package:news/src/resources/news_api_provider.dart';
 import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds returns a list of ids', () {
+    // setup of test case
+    final sum = 1 + 3;
+
+    // expectation
+    expect(sum, 4);
+  });
+}
