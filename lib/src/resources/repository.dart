67e9def8c4a432f7ef860fc449c2adc654472d48
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-16 11:38:35 +0530

    Type Annotations

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -7,11 +7,11 @@ class Repository {
   NewsDbProvider dbProvider = NewsDbProvider();
   NewsApiProvider apiProvider = NewsApiProvider();
 
-  fetchToIds() {
+  Future<List<int>> fetchToIds() {
     return apiProvider.fetchTopIds();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     var item = await dbProvider.fetchItem(id);
     if (item != null) {
       return item;
