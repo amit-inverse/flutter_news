e3146fb842c7ab75cd7c39eb734b4700c05bb80b
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-16 09:58:07 +0530

    Creating a DB Connection

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -1,12 +1,18 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
-import 'dart:io';
 import 'package:path/path.dart';
-import 'dart:async';
 import '../models/item_model.dart';
 
 class NewsDbProvider {
   late Database db;
 
-  init() async {}
+  init() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, "items.db");
+    db = await openDatabase(path, version: 1, onCreate: (Database newDb, int version) {
+      
+    });
+  }
 }
