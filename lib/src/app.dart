1c74e9e3e7e7b2029dc50735c1344d57ba43f517
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-19 08:14:57 +0530

    A Touch of Boilerplate

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import './screens/news_list.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'News',
-      // home: NewsList(),
+      home: NewsList(),
     );
   }
 }
