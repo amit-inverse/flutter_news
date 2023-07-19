5c0a898ae8d488229eab45151add1c8f95291c3a
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-19 08:11:05 +0530

    App Setup

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      title: 'News',
+      // home: NewsList(),
+    );
+  }
+}
