9212b03a29927a4eb63c74e5c135830f9bc6fa5e
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-19 22:48:11 +0530

    Futurebuilder in Action

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,42 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class NewsList extends StatelessWidget {
+  const NewsList({super.key});
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Top News'),
+        title: const Text('Top News'),
       ),
-      body: Text('Some news here!'),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return SizedBox(
+              height: 80.0,
+              child: snapshot.hasData
+                ? Text('I am visible $index')
+                : Text('I have not fetched data yet $index'),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(
+      const Duration(seconds: 2),
+      () => 'hi',
     );
   }
 }
