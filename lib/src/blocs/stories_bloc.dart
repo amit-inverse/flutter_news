b84167f8fe6ccde2dff3a3fe25493fa72f377113
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-20 08:30:04 +0530

    Exposing Bloc Getters

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -3,10 +3,16 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
+  final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
 
   // Getters to Stream
   Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchToIds();
+    _topIds.sink.add(ids!);
+  }
 
   dispose() {
     _topIds.close();
