a04a0fe9a366933c5527216ad7a296ba478ec809
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-26 22:01:35 +0530

    Finishing the ScanStreamTransformer

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
@@ -12,6 +13,16 @@ class StoriesBloc {
   fetchTopIds() async {
     final ids = await _repository.fetchToIds();
     _topIds.sink.add(ids!);
+  }
+
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel?>> cache, int id, _) {
+      cache[id] = _repository.fetchItem(id);
+      return cache;
+    },
+    <int, Future<ItemModel>>{},
+    );
   }
 
   dispose() {
