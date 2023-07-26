4c0925a46327bc5948ef54de55f8e68abf12eff3
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-26 22:41:42 +0530

    Double Gotcha

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -27,11 +27,12 @@ class StoriesBloc {
 
   _itemsTransformer() {
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel?>> cache, int id, _) {
-      cache[id] = _repository.fetchItem(id);
-      return cache;
-    },
-    <int, Future<ItemModel>>{},
+      (Map<int, Future<ItemModel?>> cache, int id, index) {
+        print(index);
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
     );
   }
 
