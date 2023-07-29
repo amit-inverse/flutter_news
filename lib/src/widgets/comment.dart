197c05df4c8860e67e8bd7a9f706a5d27feb278d
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-29 11:53:42 +0530

    Showing Individual Comments

---
+++ lib/src/widgets/comment.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+
+class Comment extends StatelessWidget {
+  final int itemId;
+  final Map<int, Future<ItemModel?>>? itemMap;
+
+  const Comment({Key? key, required this.itemId, required this.itemMap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ItemModel?>(
+      future: itemMap![itemId],
+      builder: (context, AsyncSnapshot<ItemModel?> snapshot) {
+        if(!snapshot.hasData) {
+          return const Text('Still loading comment');
+        }
+
+        return Text('${snapshot.data!.text}');
+      },
+    );
+  }
+}
